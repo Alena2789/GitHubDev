bbccec776642d881e5153318f8b3a5f117cc3b94
Alena <alena.muhamathanova.89@mail.ru>
2021-10-07 18:57:16 +0400

    Выполненная  домашняя работа

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        HomeWorkApp.printThreeWords();
+      /*  HomeWorkApp.printThreeWords();
         HomeWorkApp.checkSumSign();
         HomeWorkApp.printColor();
-        HomeWorkApp.compareNumbers();
+        HomeWorkApp.compareNumbers();*/
+       // lesson2_october.Test();
+        System.out.println("Передаем массив 4 на 4");
+        char[][] exampleRight = new char[4][4];
+        lesson2_october.Task_1(exampleRight);
+
+        System.out.println("Передаем массив 5 на 5");
+        char[][] exampleBigger = new char[5][5];
+        lesson2_october.Task_1(exampleBigger);
 
         }
 
