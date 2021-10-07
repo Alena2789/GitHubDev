3a1baf77ec0f641f7dc32419534dfafc40ee1eee
Alena <alena.muhamathanova.89@mail.ru>
2021-10-07 22:56:49 +0400

    Выполненная  домашняя работа

--- src/com/company/MyArraySizeException.java
+++ src/com/company/MyArraySizeException.java
@@ -1,4 +1,8 @@
 package com.company;
 
-public class MyArraySizeException extends Exception {
+
+public class MyArraySizeException extends BaseException {
+    MyArraySizeException() {
+        super("Ошибка. Размерность массива должна быть [4 x 4]");
+    }
 }
--- src/com/company/lesson2_october.java
+++ src/com/company/lesson2_october.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class lesson2_october {
-    public static void Test()
+   /* public static void Test()
     {
         int a, b;
         try {
@@ -12,18 +12,18 @@ public class lesson2_october {
             System.out.println("Деление на ноль");
         }
         System.out.println("Завершение работы");
-    }
+    }*/
 
-    public static void Task_1(char[][] ar) {
+    public static void Task_1(String[][] ar) {
         try {
             for (int i = 0; i < ar.length; i++) {
               //  System.out.println(ar[i].length);
                 if (i > 3) {
-                    System.out.println("Превышено количество строк в массиве, должно быть не более 4");
+                  //  System.out.println("Превышено количество строк в массиве, должно быть не более 4");
                     throw new MyArraySizeException();
                 }
                 if (ar[i].length > 4) {
-                    System.out.println("Превышено количество столбцов в массиве, должно быть не более 4");
+                  //  System.out.println("Превышено количество столбцов в массиве, должно быть не более 4");
                     throw new MyArraySizeException();
 
                 }
@@ -32,8 +32,28 @@ public class lesson2_october {
         }
         catch (MyArraySizeException e)
         {
-            System.out.println("Аргументы переданы неврено");
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    static int strToSum(String[][] ar)
+            throws MyArraySizeException, MyArrayDataException {
+
+        int sum = 0;
+
+        if (ar.length != 4) throw new MyArraySizeException();
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i].length > 4) throw new MyArraySizeException();
+            for (int y = 0; y < ar[i].length; y++) {
+                try {
+                    sum += Integer.parseInt(ar[i][y]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, y);
+                }
+            }
+        }
+
+        return sum;
     }
 }
